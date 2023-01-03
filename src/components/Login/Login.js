90d97aa94d6e42fbed828f60b306c0594f5acfa2
jsguerra <jose@gcostudios.com>
2023-01-03 20:44:36 +0000

    useEffect Cleanup function lesson 113

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    // debounce
+    const identifier = setTimeout(() => {
+      console.log("Checking form validity");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log("Cleanup");
+      clearTimeout(identifier);
+    }; // Cleanup function
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
